58f9422ead1e0239ad7ad0d2eebd96aeb0352fc7
Duc Dat <phamducdat14122000@gmail.com>
2022-03-02 08:48:40 +0700

    feature/update_api_delete_message

--- app/chat/api/urls.py
+++ app/chat/api/urls.py
@@ -11,19 +11,5 @@ urlpatterns = [
          views.ChangeGroupNameChatTag.as_view()),
     path('change-chat-color/<str:chat_tag>', views.ChangeChatColor.as_view()),
     path('list-info-user', views.GetListUserInfo.as_view()),
-    path('delete-message/<str:id_message>', views.DeleteMessage.as_view())
+    path('delete-message', views.DeleteMessage.as_view())
 ]
-
-# handle chat tag
-#     path('handle-chat-tag', views.HandleChatTag.as_view()),
-#     path('handle-chat-tag-enjoy', views.HandleChatTagEnjoy.as_view()),
-
-# handle bubble palace
-#     path('handle-bubble-palace', views.HandleBubblePalace.as_view()),
-# path('handle-bubble-palace-enjoy', views.HandleBubblePlaceForEnjoy.as_view()),
-
-#     path('request-public/<str:chat_tag>', views.RequestPublic.as_view()),
-# path('agree-request-public/<str:chat_tag>',
-#      views.AgreeRequestPublic.as_view()),
-
-#     path('send-message', views.HandleMessage.as_view()),
