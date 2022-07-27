01d558576ef20ca6ea9a300ffc44829b677deec9
datphamamela <phamducdat14122000@gmail.com>
2022-07-27 15:11:20 +0700

    feat/increase_timeout_mongo

--- chat/mongoDb.ts
+++ chat/mongoDb.ts
@@ -1,7 +1,7 @@
 import { MongoClient } from "mongodb";
 import env from "./env";
 
-const uri = `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=2000&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
+const uri = `mongodb://${env.MONGO_USER}:${env.MONGO_PASSWORD}@${env.MONGO_HOST}:27017/?authSource=admin&readPreference=primary&serverSelectionTimeoutMS=15000&appname=MongoDB%20Compass&directConnection=true&ssl=false`;
 
 const client = new MongoClient(uri);
 client.connect();
