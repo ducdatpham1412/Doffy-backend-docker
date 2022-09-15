99d4eeb55ad8636f77419448796f1ea38fb8b4bb
datphamamela <phamducdat14122000@gmail.com>
2022-09-15 11:53:20 +0700

    feat/add_reputations_in_profile_serializer

--- app/myprofile/serializers.py
+++ app/myprofile/serializers.py
@@ -1,6 +1,8 @@
 from utilities.services import create_link_image
 from rest_framework import serializers
 from myprofile import models
+from findme.mongo import mongoDb
+from utilities.enums import total_reputation
 
 
 class ProfileSerializer(serializers.ModelSerializer):
@@ -9,13 +11,10 @@ class ProfileSerializer(serializers.ModelSerializer):
     cover = serializers.SerializerMethodField()
     followers = serializers.SerializerMethodField()
     followings = serializers.SerializerMethodField()
-    anonymousName = serializers.SerializerMethodField()
+    reputations = serializers.SerializerMethodField()
 
     def get_id(self, obj):
         return obj.user.id
-
-    def get_anonymousName(self, obj):
-        return obj.anonymous_name
 
     def get_avatar(self, obj):
         return create_link_image(obj.avatar)
@@ -31,10 +30,19 @@ class ProfileSerializer(serializers.ModelSerializer):
         count = models.Follow.objects.filter(follower=obj.user.id).count()
         return count
 
+    def get_reputations(self, obj):
+        total = mongoDb.total_items.find_one({
+            'type': total_reputation,
+            'user_id': obj.user.id,
+        })
+        if not total:
+            return 0
+        return total['value']
+
     class Meta:
         model = models.Profile
-        fields = ['id', 'name', 'anonymousName', 'avatar', 'cover',
-                  'description', 'followers', 'followings']
+        fields = ['id', 'name', 'avatar', 'cover',
+                  'description', 'followers', 'followings', 'reputations']
         read_only_fields = ['user']
 
 
